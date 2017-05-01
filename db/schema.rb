58fa4ee611840935beb14637f912f68c2bd6e88a
alexander <alexanderkalife@gmail.com>
2017-05-01 11:25:53 -0400

    migrated the reports table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430165804) do
+ActiveRecord::Schema.define(version: 20170501145942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reports", force: :cascade do |t|
+    t.text     "body",       null: false
+    t.integer  "spot_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spot_id"], name: "index_reports_on_spot_id", using: :btree
+    t.index ["user_id"], name: "index_reports_on_user_id", using: :btree
+  end
 
   create_table "spots", force: :cascade do |t|
     t.string   "name",                        null: false
