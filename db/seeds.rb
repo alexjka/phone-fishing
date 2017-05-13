0d7963e0eca11c76246b8a6c34540b438720e8f1
alexander <alexanderkalife@gmail.com>
2017-05-13 14:11:40 -0400

    more for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Spot.create!(
+  name: "Penikese",
+  description: "just between Penikese and Cuttyhunk",
+  user_id: 1,
+  lat: "41.4393",
+  long: "70.9275"
+  )
+Spot.create!(
+  name: "Weepeckets",
+  description: "just east of Robinsons",
+  user_id: 1,
+  lat: "41.5168",
+  long: "70.7356"
+  )
